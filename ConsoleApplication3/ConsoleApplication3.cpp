68cc1abd3e161547f460a0a182c5d61269f967b9
maximus2288 <84533536+maximus2288@users.noreply.github.com>
2022-09-01 17:24:42 +0300

    hello world
    hello world

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 #include <string>
 #include <math.h>
@@ -8,45 +8,10 @@ using namespace std;
 
 
 
-int cn(int a) {
-    int first = 0;
-    int second;
-    int num = 0;
-    int d = 1000000000;
-    int del = 0;
-    if (a < 100)
-    {
-        return -3;
-    }
-    else {
-        while (first == 0)
-        {
-            num = a / d;
-            if (num != 0 & first == 0) {
-                first = num;
-                del = d;
-            }
-            d = d / 10;
-
-        }
-        cout << first << "   " << del;
-    }
-}
-
 int main()
 {
-    int a;
-    cin >> a;
-    int c = cn(a);
-    / if (c == -3) {
-        cout << "Нету 3 числа";
-    }
-    else {
-        cout << c;
-    } /
-
-
-
+ 
+cout <<"hello world";
 
 }
 
